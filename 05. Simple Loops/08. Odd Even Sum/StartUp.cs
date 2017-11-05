9847099c6210ebfeeb6ee25301d2d1a6aa70b742
Teodor <thelad43@gmail.com>
2017-11-05 20:30:07 +0200

    Added solution for problem 08. Odd Even Sum

---
+++ 05. Simple Loops/08. Odd Even Sum/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace _08.OddEvenSum
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var evenSum = 0;
+            var oddSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum += int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    oddSum += int.Parse(Console.ReadLine());
+                }
+            }
+
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(oddSum - evenSum)}");
+            }
+        }
+    }
+}
