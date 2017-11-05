a0997e25b44fcbd78033f5e978453fd053a2ca21
Teodor <thelad43@gmail.com>
2017-11-05 19:26:44 +0200

    Added solution for problem 08. Triangle Area

---
+++ Simple Calculations/08. Triangle Area/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _08.Triangle_Area
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var side = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:F2}", side * height / 2);
+        }
+    }
+}
