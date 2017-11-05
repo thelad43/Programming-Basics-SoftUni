9aebf23bbd1cbbfecef350bd23faf9febb4e687e
Teodor <thelad43@gmail.com>
2017-11-05 18:56:16 +0200

    Added solution for problem 03. Greeting by Name

---
+++ Simple Calculations/03. Greeting by Name/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _03.Greeting_by_Name
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
