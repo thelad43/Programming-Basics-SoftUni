4fbd2c095b91eb316c8f8955f31f8a33fa7891ba
Teodor <thelad43@gmail.com>
2017-11-05 18:36:57 +0200

    Added solution "First Steps in Coding.sln" and 01. Hello SoftUni problem

---
+++ First Steps in Coding/01. Hello SoftUni/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace _01.Hello_SoftUni
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello C#");
+        }
+    }
+}
