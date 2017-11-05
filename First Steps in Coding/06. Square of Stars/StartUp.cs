7e3c3081623a6decef23271e6336a11c4fff1204
Teodor <thelad43@gmail.com>
2017-11-05 18:44:37 +0200

    Added solution for problem 06. Square of Stars

---
+++ First Steps in Coding/06. Square of Stars/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _06.Square_of_Stars
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new String('*', n));
+
+            for (int i = 1; i <= n - 2; i++)
+            {
+                Console.WriteLine("*{0}*", new string(' ', n - 2));
+            }
+
+            Console.WriteLine(new String('*', n));
+        }
+    }
+}
