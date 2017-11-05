a508c51ae9b9dc222a6484e9d9358cec1a64cfd9
Teodor <thelad43@gmail.com>
2017-11-05 19:42:49 +0200

    Added solution for problem 04. Greater Number

---
+++ 03. Simple Conditional Statements/04. Greater Number/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.Greater_Number
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else
+            {
+                Console.WriteLine(secondNumber);
+            }
+        }
+    }
+}
