5d47bd05deb957de6f31d283638e7f52ef230efd
Teodor <thelad43@gmail.com>
2017-11-05 19:24:57 +0200

    Added solution for problem 07. 2D Rectangle Area

---
+++ Simple Calculations/07. 2D Rectangle Area/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _07._2D_Rectangle_Area
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+
+            var a = Math.Abs(x1 - x2);
+            var b = Math.Abs(y1 - y2);
+
+            Console.WriteLine(a * b);
+            Console.WriteLine(2 * (a + b));
+        }
+    }
+}
