1664f24147b080ea5548dc37c5a1a110f65e253b
Teodor <thelad43@gmail.com>
2017-11-05 20:15:48 +0200

    Added solution for problem 10. Animal Type

---
+++ 04. Complex Conditional Statements/10. Animal Type/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _10.Animal_Type
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var animial = Console.ReadLine();
+
+            if (animial == "crocodile" || animial == "tortoise" || animial == "snake")
+            {
+                Console.WriteLine("reptile");
+            }
+            else if (animial == "dog")
+            {
+                Console.WriteLine("mammal");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
