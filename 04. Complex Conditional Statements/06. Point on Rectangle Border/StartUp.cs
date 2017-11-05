f915a413154a973d8be5cdbb5cbda82530291da0
Teodor <thelad43@gmail.com>
2017-11-05 20:11:45 +0200

    Added solution for problem 06. Point on Rectangle Border

---
+++ 04. Complex Conditional Statements/06. Point on Rectangle Border/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _06.Point_on_Rectangle_Border
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+            var x = double.Parse(Console.ReadLine());
+            var y = double.Parse(Console.ReadLine());
+
+            if ((x == x1 || x == x2) && (y > y1 && y <= y2) ||
+                (y == y1 || y == y2) && (x > x1 && x <= x2))
+            {
+                Console.WriteLine("Border");
+            }
+            else
+            {
+                Console.WriteLine("Inside / Outside");
+            }
+        }
+    }
+}
