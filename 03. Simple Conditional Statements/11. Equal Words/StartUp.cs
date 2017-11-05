08ae8479d7d170baf01667b6f9fc8b23b4942b01
Teodor <thelad43@gmail.com>
2017-11-05 19:53:46 +0200

    Added solution for problem 11. Equal Words

---
+++ 03. Simple Conditional Statements/11. Equal Words/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _11.Equal_Words
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstWord = Console.ReadLine().ToLower();
+            var secondWord = Console.ReadLine().ToLower();
+
+            if (firstWord == secondWord)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
