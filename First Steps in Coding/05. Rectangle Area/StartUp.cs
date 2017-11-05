390b4ef5ea7335eb302be0a733f5211934a9333c
Teodor <thelad43@gmail.com>
2017-11-05 18:43:36 +0200

    Added solution for problem 05. Rectangle Area

---
+++ First Steps in Coding/05. Rectangle Area/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _05.Rectangle_Area
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var result = a * b;
+            Console.WriteLine(result);
+        }
+    }
+}
