8152c6991deb9c1f36c6f4b23750e994e5c6da67
Teodor <thelad43@gmail.com>
2017-11-05 19:52:52 +0200

    Added solution for problem 10. Number 100...200

---
+++ 03. Simple Conditional Statements/10. Number 100 To 200/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _10.Number_100_To_200
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            if (num > 200)
+            {
+                Console.WriteLine("Greater than {0}", 200);
+            }
+            else if (num < 100)
+            {
+                Console.WriteLine("Less than {0}", 100);
+            }
+            else
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+        }
+    }
+}
