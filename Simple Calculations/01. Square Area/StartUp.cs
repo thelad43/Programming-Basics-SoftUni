4688a73887372687f47c2b3c0c3451d59f1ccd33
Teodor <thelad43@gmail.com>
2017-11-05 18:53:14 +0200

    Added solution "Simple Calculations.sln" and solution for problem 01. Square Area

---
+++ Simple Calculations/01. Square Area/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.Square_Area
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var side = double.Parse(Console.ReadLine());
+            Console.WriteLine(side * side);
+        }
+    }
+}
