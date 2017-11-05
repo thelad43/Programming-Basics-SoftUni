b3ee0e28709df9b64289087712d042dc6411cd20
Teodor <thelad43@gmail.com>
2017-11-05 20:25:33 +0200

    Added solution for problem 03. Latin Letters

---
+++ 05. Simple Loops/03. Latin Letters/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _03.LatinLetters
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
