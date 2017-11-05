a3b6af87790dce393e77ed553b04227d30ae99ce
Teodor <thelad43@gmail.com>
2017-11-05 19:38:41 +0200

    Added 03. Simple Conditional Statements solution and problem 01. Excellent Result

---
+++ 03. Simple Conditional Statements/01. Excellent Result/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace Excellent_Result
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
