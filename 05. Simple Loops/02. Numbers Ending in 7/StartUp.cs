54588363214fa7b79f084878f61274a4c124fc13
Teodor <thelad43@gmail.com>
2017-11-05 20:23:43 +0200

    Added solution for problem 02. Numbers Ending in 7

---
+++ 05. Simple Loops/02. Numbers Ending in 7/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.NumbersEndingIn7
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            for (int i = 7; i <= 997; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
