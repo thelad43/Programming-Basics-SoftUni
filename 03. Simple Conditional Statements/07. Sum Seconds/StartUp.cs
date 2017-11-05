ad561036013e562493bf966f28f299847d5a569a
Teodor <thelad43@gmail.com>
2017-11-05 19:48:54 +0200

    Added solution for problem 07. Sum Seconds

---
+++ 03. Simple Conditional Statements/07. Sum Seconds/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _07.Sum_Seconds
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstTimeInSeconds = int.Parse(Console.ReadLine());
+            var secondTimeInSeconds = int.Parse(Console.ReadLine());
+            var thirdTimeInSeconds = int.Parse(Console.ReadLine());
+
+            var sum = new DateTime();
+
+            sum = sum.AddSeconds(firstTimeInSeconds);
+            sum = sum.AddSeconds(secondTimeInSeconds);
+            sum = sum.AddSeconds(thirdTimeInSeconds);
+
+            Console.WriteLine(sum.ToString("m:ss"));
+        }
+    }
+}
