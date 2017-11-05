2e854950ff499779b7899b350240f5114e64ca76
Teodor <thelad43@gmail.com>
2017-11-05 19:51:12 +0200

    Added solution for problem 09. Password Guess

---
+++ 03. Simple Conditional Statements/09. Password Guess/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _09.Password_Guess
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var pass = Console.ReadLine();
+
+            if (pass == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Password!");
+            }
+        }
+    }
+}
