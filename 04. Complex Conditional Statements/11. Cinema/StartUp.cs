d17e7a542e62fd079220548f25eadc10074ac24e
Teodor <thelad43@gmail.com>
2017-11-05 20:16:38 +0200

    Added solution for problem 11. Cinema

---
+++ 04. Complex Conditional Statements/11. Cinema/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace _11.Cinema
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var projection = Console.ReadLine().ToLower();
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+            var money = 0d;
+
+            if (projection == "premiere")
+            {
+                money = rows * cols * 12;
+            }
+
+            else if (projection == "normal")
+            {
+                money = rows * cols * 7.50;
+            }
+            else if (projection == "discount")
+            {
+                money = rows * cols * 5;
+            }
+
+            Console.WriteLine("{0:F2} leva", money);
+        }
+    }
+}
