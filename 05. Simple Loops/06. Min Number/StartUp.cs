683ac6520476339f27457acf4f2187a7d86a1aac
Teodor <thelad43@gmail.com>
2017-11-05 20:28:04 +0200

    Added solution for problem 06. Min Number

---
+++ 05. Simple Loops/06. Min Number/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _06.MinNumber
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var smallestNumber = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber < smallestNumber)
+                {
+                    smallestNumber = currentNumber;
+                }
+            }
+
+            Console.WriteLine(smallestNumber);
+        }
+    }
+}
