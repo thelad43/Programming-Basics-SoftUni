ad550b67b4af82849409ca886b65e9d77b080819
Teodor <thelad43@gmail.com>
2017-11-05 18:54:33 +0200

    Added solution for problem 02. Inches to Centimeters

---
+++ Simple Calculations/02. Inches to Centimeters/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _02.Inches_to_Centimeters
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inches = double.Parse(Console.ReadLine());
+            Console.WriteLine(inches * 2.54);
+        }
+    }
+}
