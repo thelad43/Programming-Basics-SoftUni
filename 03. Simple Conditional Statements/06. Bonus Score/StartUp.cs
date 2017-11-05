accc21a32cc53c5b382b01ab44d26bc5fcfec819
Teodor <thelad43@gmail.com>
2017-11-05 19:46:29 +0200

    Added solution for problem 06. Bonus Score

---
+++ 03. Simple Conditional Statements/06. Bonus Score/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace _06._Bonus_Score
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+            var bonusScore = 0.0;
+
+            if (num <= 100)
+            {
+                bonusScore = 5;
+            }
+
+            if (num > 100 && num <= 1000)
+            {
+                bonusScore += num * 0.2;
+            }
+            else if (num > 1000)
+            {
+                bonusScore += num * 0.1;
+            }
+
+            if (num % 2 == 0)
+            {
+                bonusScore += 1;
+            }
+
+            if (num % 10 == 5)
+            {
+                bonusScore += 2;
+            }
+
+            Console.WriteLine(bonusScore);
+            Console.WriteLine(bonusScore + num);
+        }
+    }
+}
