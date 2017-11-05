1abcae99fb1b8aad9c3772e053b51e88c8fc2dbd
Teodor <thelad43@gmail.com>
2017-11-05 19:29:15 +0200

    Added solution for problem 10. Radians to Degrees

---
+++ Simple Calculations/10. Radians to Degrees/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _10.Radians_to_Degrees
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var rad = double.Parse(Console.ReadLine());
+            var degs = rad * 180 / Math.PI;
+            Console.WriteLine(Math.Round(degs));
+        }
+    }
+}
