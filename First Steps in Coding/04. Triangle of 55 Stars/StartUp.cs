0cc529b168d541c3f6125ff73934a7b892010a6a
Teodor <thelad43@gmail.com>
2017-11-05 18:42:23 +0200

    Added solution for problem 04. Triangle of 55 Stars

---
+++ First Steps in Coding/04. Triangle of 55 Stars/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.Triangle_of_55_Stars
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
