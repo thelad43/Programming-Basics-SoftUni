20eee52b73a5c033517968731584ee8ac016442e
Teodor <thelad43@gmail.com>
2017-11-05 20:31:52 +0200

    Added solution for problem 10. Half Sum Element

---
+++ 05. Simple Loops/10. Half Sum Element/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _10.HalfSumElement
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var num in numbers)
+            {
+                if (num == (numbers.Sum() - num))
+                {
+                    Console.WriteLine("Yes");
+                    Console.WriteLine($"Sum = {numbers.Sum() - num}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("No");
+            Console.Write("Diff = ");
+            Console.WriteLine($"{Math.Abs(numbers.Max() - (numbers.Sum() - numbers.Max()))}");
+        }
+    }
+}
