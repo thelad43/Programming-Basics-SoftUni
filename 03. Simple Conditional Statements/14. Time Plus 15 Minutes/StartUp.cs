05f14aaa2d32f88f36a03bb513624b25e310dc47
Teodor <thelad43@gmail.com>
2017-11-05 19:57:14 +0200

    Added solution for problem 14. Time + 15 Minutes

---
+++ 03. Simple Conditional Statements/14. Time Plus 15 Minutes/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace _14.Time_Plus_15_Minutes
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+
+            var afterFifteenMinutes = new DateTime();
+
+            afterFifteenMinutes = afterFifteenMinutes.AddHours(hours);
+            afterFifteenMinutes = afterFifteenMinutes.AddMinutes(minutes + 15);
+
+            var result = string.Empty;
+
+            if (afterFifteenMinutes.Minute <= 9)
+            {
+                result = afterFifteenMinutes.Hour + ":0" + afterFifteenMinutes.Minute;
+            }
+            else
+            {
+                result = afterFifteenMinutes.Hour + ":" + afterFifteenMinutes.Minute;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
