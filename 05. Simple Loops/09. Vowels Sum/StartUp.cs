bd060409b2d881166f58668d2230ba06cce3ef19
Teodor <thelad43@gmail.com>
2017-11-05 20:31:13 +0200

    Added solution for problem 09. Vowels Sum

---
+++ 05. Simple Loops/09. Vowels Sum/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace _09.VowelsSum
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var allLeters = Console.ReadLine().ToCharArray();
+            var sum = 0;
+
+            foreach (var letter in allLeters)
+            {
+                switch (letter)
+                {
+                    case 'a':
+                        sum += 1;
+                        break;
+                    case 'e':
+                        sum += 2;
+                        break;
+                    case 'i':
+                        sum += 3;
+                        break;
+                    case 'o':
+                        sum += 4;
+                        break;
+                    case 'u':
+                        sum += 5;
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
