9d1e029350448fa226d13992c9c2113490887da0
Teodor <thelad43@gmail.com>
2017-11-05 19:41:13 +0200

    Added solution for problem 03. Even or Odd

---
+++ 03. Simple Conditional Statements/03. Even or Odd/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.Even_or_Odd
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
