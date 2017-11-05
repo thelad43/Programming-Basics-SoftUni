455a37b1c9af45b1d1d5dcbc07e9d06e49243b78
Teodor <thelad43@gmail.com>
2017-11-05 19:32:50 +0200

    Added solution for problem 13. 1000 Days After Birth

---
+++ Simple Calculations/13. 1000 Days After Birth/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _13._1000_Days_After_Birth
+{
+    using System;
+    using System.Globalization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var date = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            date = date.AddDays(999);
+            Console.WriteLine(date.ToString("dd-MM-yyyy"));
+        }
+    }
+}
