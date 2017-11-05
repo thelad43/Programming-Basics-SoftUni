14dc1d2b348c588ddbe7f9d502541c9751cccca2
Teodor <thelad43@gmail.com>
2017-11-05 20:27:01 +0200

    Added solution for problem 05. Max Number

---
+++ 05. Simple Loops/05. Max Number/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.MaxNumber
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var biggestNum = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber > biggestNum)
+                {
+                    biggestNum = currentNumber;
+                }
+            }
+
+            Console.WriteLine(biggestNum);
+        }
+    }
+}
