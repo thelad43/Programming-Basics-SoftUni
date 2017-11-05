87fbaa6af1dae10fd3498788c2b197a372dfdcd7
Teodor <thelad43@gmail.com>
2017-11-05 19:58:36 +0200

    Added solution for problem 15. 3 Equal Numbers

---
+++ 03. Simple Conditional Statements/15. 3 Equal Numbers/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _15._3_Equal_Numbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+
+            if (a == b && b == c)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
