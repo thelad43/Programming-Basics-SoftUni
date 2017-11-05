10bd439e17b70ba89a709d2b9a73dc1427bd9358
Teodor <thelad43@gmail.com>
2017-11-05 19:27:49 +0200

    Added solution for problem 09. Celsius to Fahrenheit

---
+++ Simple Calculations/09. Celsius to Fahrenheit/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _09.Celsius_to_Fahrenheit
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var celsius = double.Parse(Console.ReadLine());
+            var fahrenheit = celsius * 1.8 + 32;
+            Console.WriteLine(fahrenheit);
+        }
+    }
+}
