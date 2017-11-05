f0535dd0d6366748b91fa6242d1e8effe9e8c22f
Teodor <thelad43@gmail.com>
2017-11-05 20:29:22 +0200

    Added solution for problem 07. Left and Right Sum

---
+++ 05. Simple Loops/07. Left and Right Sum/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _07.LeftAndRightSum
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var leftSum = 0;
+            var rightSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                leftSum += int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                rightSum += int.Parse(Console.ReadLine());
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+            }
+        }
+    }
+}
