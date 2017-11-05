e29ef1726af3b4d948c1f5f4d5b6a1c150fe0c44
Teodor <thelad43@gmail.com>
2017-11-05 20:17:25 +0200

    Added solution for problem 12. Volleyball

---
+++ 04. Complex Conditional Statements/12. Volleyball/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _12.Volleyball
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var isLeapOrNot = Console.ReadLine();
+            var holidays = double.Parse(Console.ReadLine());
+            var weekendsTravelToBornTown = int.Parse(Console.ReadLine());
+
+            var yearWeekends = 48;
+            var nonWork = 3d / 4d;
+            var daysOfHoliday = 2d / 3d;
+            var percanteges = 15d;
+
+            var weekendsInSofia = (double)yearWeekends - weekendsTravelToBornTown;
+            weekendsInSofia *= nonWork;
+            holidays *= daysOfHoliday;
+            var all = weekendsInSofia + holidays + weekendsTravelToBornTown;
+            var result = 0d;
+
+            if (isLeapOrNot == "leap")
+            {
+                result = percanteges * all / 100d;
+                result += all;
+                Console.WriteLine(Math.Floor(result));
+            }
+            else
+            {
+                Console.WriteLine(Math.Floor(all));
+            }
+        }
+    }
+}
