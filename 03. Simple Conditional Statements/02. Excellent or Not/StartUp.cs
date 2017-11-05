e8ef17c369f45fad2ef2126aa1ee780fcf65975c
Teodor <thelad43@gmail.com>
2017-11-05 19:40:09 +0200

    Added solution for problem 02. Excellent or Not

---
+++ 03. Simple Conditional Statements/02. Excellent or Not/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace Excellent_or_Not
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else
+            {
+                Console.WriteLine("Not excellent.");
+            }
+        }
+    }
+}
