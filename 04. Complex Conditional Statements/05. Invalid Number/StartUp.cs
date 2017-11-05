483f5fac52dc2791d169fcd8a58f08b72e612b06
Teodor <thelad43@gmail.com>
2017-11-05 20:10:44 +0200

    Added solution for problem 05. Invalid Number

---
+++ 04. Complex Conditional Statements/05. Invalid Number/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _05.Invalid_Number
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            if (!((num >= 100 && num <= 200) || num == 0))
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
