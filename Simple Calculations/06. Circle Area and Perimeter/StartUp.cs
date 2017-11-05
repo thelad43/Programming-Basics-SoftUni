c04f02c39e790fedfa9e3da761cd3cf8aad79554
Teodor <thelad43@gmail.com>
2017-11-05 19:23:49 +0200

    Added solution for problem 06. Circle Area and Perimeter

---
+++ Simple Calculations/06. Circle Area and Perimeter/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _06.Circle_Area_and_Perimeter
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var radius = double.Parse(Console.ReadLine());
+            var area = Math.PI * radius * radius;
+            var perimeter = 2 * Math.PI * radius;
+
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+        }
+    }
+}
