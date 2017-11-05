3ca43dcde865de1c6abffd1d8db78b87faf2f21c
Teodor <thelad43@gmail.com>
2017-11-05 20:33:28 +0200

    Added solution for problem 11. Odd / Even Position

---
+++ 05. Simple Loops/11. Odd Or Even Position/StartUp.cs
@@ -0,0 +1,97 @@
+﻿namespace _11.OddEvenPosition
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = double.Parse(Console.ReadLine());
+            }
+
+            double oddSum = 0;
+            double oddMin = int.MaxValue;
+            double oddMax = int.MinValue;
+            double evenSum = 0;
+            double evenMin = int.MaxValue;
+            double evenMax = int.MinValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!(i % 2 == 1))
+                {
+                    oddSum += numbers[i];
+
+                    if (numbers[i] < oddMin)
+                    {
+                        oddMin = numbers[i];
+                    }
+
+                    if (numbers[i] > oddMax)
+                    {
+                        oddMax = numbers[i];
+                    }
+                }
+                else
+                {
+                    evenSum += numbers[i];
+
+                    if (numbers[i] < evenMin)
+                    {
+                        evenMin = numbers[i];
+                    }
+
+                    if (numbers[i] > evenMax)
+                    {
+                        evenMax = numbers[i];
+                    }
+                }
+            }
+
+            Console.WriteLine($"OddSum={oddSum}");
+
+            if (oddMin < int.MaxValue)
+            {
+                Console.WriteLine($"OddMin={oddMin}");
+            }
+            else
+            {
+                Console.WriteLine($"OddMin=No");
+            }
+
+            if (oddMax > int.MinValue)
+            {
+                Console.WriteLine($"OddMax={oddMax}");
+            }
+            else
+            {
+                Console.WriteLine($"OddMax=No");
+            }
+
+            Console.WriteLine($"EvenSum={evenSum}");
+
+            if (evenMin != int.MaxValue)
+            {
+                Console.WriteLine($"EvenMin={evenMin}");
+            }
+            else
+            {
+                Console.WriteLine($"EvenMin=No");
+            }
+
+            if (evenMax != int.MinValue)
+            {
+                Console.WriteLine($"EvenMax={evenMax}");
+            }
+            else
+            {
+                Console.WriteLine($"EvenMax=No");
+            }
+        }
+    }
+}
