f0d6d1c60a9956223726686fc0a988eb80b3199d
Teodor <thelad43@gmail.com>
2017-11-05 19:30:11 +0200

    Added solution for problem 11. USD to BGN

---
+++ Simple Calculations/11. USD to BGN/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _11.USD_to_BGN
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dollar = 1.79549;
+            var dollarsToConvert = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:F2}", dollarsToConvert * dollar);
+        }
+    }
+}
