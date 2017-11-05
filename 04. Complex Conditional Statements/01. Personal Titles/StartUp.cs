c787c8ef0f0b5a04bd200a66b68ad9d8b97d3a62
Teodor <thelad43@gmail.com>
2017-11-05 20:03:07 +0200

    Added 04. Complex Conditional Statements.sln and problem 01. Personal Titles

---
+++ 04. Complex Conditional Statements/01. Personal Titles/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.Personal_Titles
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var age = double.Parse(Console.ReadLine());
+            var gender = Console.ReadLine();
+
+            if (gender == "m" && age >= 16)
+            {
+                Console.WriteLine("Mr.");
+            }
+            else if (gender == "m" && age < 16)
+            {
+                Console.WriteLine("Master");
+            }
+            else if (gender == "f" && age >= 16)
+            {
+                Console.WriteLine("Ms.");
+            }
+            else
+            {
+                Console.WriteLine("Miss");
+            }
+        }
+    }
+}
