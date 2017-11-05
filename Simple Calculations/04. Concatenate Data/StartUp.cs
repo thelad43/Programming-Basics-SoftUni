1cd786615377bfe7a372c4301cba374960d8bc94
Teodor <thelad43@gmail.com>
2017-11-05 18:58:50 +0200

    Added solution for problem 04. Concatenate Data

---
+++ Simple Calculations/04. Concatenate Data/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.Concatenate_Data
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstName = Console.ReadLine();
+            var lastName = Console.ReadLine();
+            var age = Console.ReadLine();
+            var town = Console.ReadLine();
+
+            Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+        }
+    }
+}
