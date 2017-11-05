4be7908c71b4cd0cd0102d7ef0fac583f7752b61
Teodor <thelad43@gmail.com>
2017-11-05 20:26:22 +0200

    Added solution for problem 04. Sum Numbers

---
+++ 05. Simple Loops/04. Sum Numbers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.SumNumbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+                sum += currentNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
