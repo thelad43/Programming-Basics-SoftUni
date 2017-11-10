6abbbe0783bf6025c7043b414316926e0533599b
Teodor <thelad43@gmail.com>
2017-11-10 18:17:33 +0200

    Added solution for problem 06. Rhombus of Stars

---
+++ 06. Drawing Figures with Loops/06. RhombusOfStars/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace _06.RhombusOfStars
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                Console.Write(new string(' ', n - row));
+                Console.Write("*");
+
+                for (int col = 1; col < row; col++)
+                {
+                    Console.Write(" *");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int row = n - 1; row > 0; row--)
+            {
+                Console.Write(new string(' ', n - row));
+                Console.Write("*");
+
+                for (int col = 1; col < row; col++)
+                {
+                    Console.Write(" *");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
