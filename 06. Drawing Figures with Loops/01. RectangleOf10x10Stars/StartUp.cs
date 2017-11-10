ee7fd6264c18c37783329b54864f6a64fe4d53e4
Teodor <thelad43@gmail.com>
2017-11-10 18:12:26 +0200

    Added 06. Drawing Figures with Loops solution and problem 01. Rectangle of 10 x 10 Stars

---
+++ 06. Drawing Figures with Loops/01. RectangleOf10x10Stars/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace RectangleOf10X10Stars
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(new string('*', 10));
+            }
+        }
+    }
+}
