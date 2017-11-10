7b84cdeb44f8eec521cee5dd47590a3d9804c8da
Teodor <thelad43@gmail.com>
2017-11-10 18:18:14 +0200

    Added solution for problem 07. Christmas Tree

---
+++ 06. Drawing Figures with Loops/07. ChristmasTree/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07.Christmas_Tree
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var stars = "*";
+
+            Console.WriteLine(new string(' ', n + 1) + "|");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string(' ', n - 1 - i) + stars + " | " + stars);
+                stars += "*";
+            }
+        }
+    }
+}
