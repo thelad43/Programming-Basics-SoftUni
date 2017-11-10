048846ee7b2b4d76cbfd4e4d7fde99dc8d502ed2
Teodor <thelad43@gmail.com>
2017-11-10 18:22:39 +0200

    Added solution for problem 09. House

---
+++ 06. Drawing Figures with Loops/09. House/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace _09.House
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var stars = n % 2 == 0 ? "**" : "*";
+
+            for (int i = 0; i < (n + 1) / 2; i++)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine(new string('-', ((n - 2) / 2) - i) + stars + new string('-', ((n - 2) / 2) - i));
+                }
+                else
+                {
+                    Console.WriteLine(new string('-', ((n - 1) / 2) - i) + stars + new string('-', ((n - 1) / 2) - i));
+                }
+
+                stars += "**";
+            }
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                Console.WriteLine("|" + new string('*', n - 2) + "|");
+            }
+        }
+    }
+}
