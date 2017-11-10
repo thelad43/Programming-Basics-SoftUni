ee503ea3384ccaf6dfc41fa54ef9a52374c9ffc2
Teodor <thelad43@gmail.com>
2017-11-10 18:14:35 +0200

    Added solution for problem 03. Square of Stars

---
+++ 06. Drawing Figures with Loops/03. SquareOfStars/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.SquareOfStars
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
