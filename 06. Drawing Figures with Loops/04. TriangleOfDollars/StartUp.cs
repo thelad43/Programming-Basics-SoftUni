ac4371807ebff2797e2bbde225bcf3bf5b902016
Teodor <thelad43@gmail.com>
2017-11-10 18:15:44 +0200

    Added solution for problem 04. Triangle of Dollars

---
+++ 06. Drawing Figures with Loops/04. TriangleOfDollars/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _04.TriangleOfDollars
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("$ ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
