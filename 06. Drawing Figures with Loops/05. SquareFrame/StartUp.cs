002e96f9f6aac1f9386a4e1d35fe711bbdba5f0e
Teodor <thelad43@gmail.com>
2017-11-10 18:16:39 +0200

    Added solution for problem 05. Square Frame

---
+++ 06. Drawing Figures with Loops/05. SquareFrame/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace _05.SquareFrame
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.Write("+ ");
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("- ");
+            }
+            Console.WriteLine("+");
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("| ");
+
+                for (int k = 0; k < n - 2; k++)
+                {
+                    Console.Write("- ");
+                }
+
+                Console.WriteLine("|");
+            }
+
+            Console.Write("+ ");
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("- ");
+            }
+            Console.WriteLine("+");
+        }
+    }
+}
