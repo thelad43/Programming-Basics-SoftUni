fd91ade4ec5f0a0d29edbd4e39406c319a20fe3b
Teodor <thelad43@gmail.com>
2017-11-10 18:20:43 +0200

    Added solution for problem 08. Sunglasses

---
+++ 06. Drawing Figures with Loops/08. Sunglasses/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _08.Sunglasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('*', n * 2) + new string(' ', n) + new string('*', n * 2));
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                if (i == (n - 1) / 2 - 1)
+                {
+                    Console.WriteLine("*" + new string('/', n * 2 - 2) + "*" + new string('|', n) + "*" + new string('/', n * 2 - 2) + "*");
+                }
+                else
+                {
+                    Console.WriteLine("*" + new string('/', n * 2 - 2) + "*" + new string(' ', n) + "*" + new string('/', n * 2 - 2) + "*");
+                }
+            }
+
+            Console.WriteLine(new string('*', n * 2) + new string(' ', n) + new string('*', n * 2));
+        }
+    }
+}
