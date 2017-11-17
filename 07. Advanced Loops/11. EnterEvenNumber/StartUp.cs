7d2f94bd553947f4d5a5f575853b6f6a9381eabf
Teodor <thelad43@gmail.com>
2017-11-17 16:09:53 +0200

    Added solution for problem 11. Enter Even Number

---
+++ 07. Advanced Loops/11. EnterEvenNumber/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _11.EnterEvenNumber
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter even number: ");
+                    n = int.Parse(Console.ReadLine());
+
+                    if (n % 2 == 1)
+                    {
+                        Console.WriteLine("The number is not even.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Even number entered: {n}");
+                        break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid number!");
+                }
+            }
+        }
+    }
+}
