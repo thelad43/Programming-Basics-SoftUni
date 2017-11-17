28a6794ceae38f9c1afa2ec16c4176fe667fe5d2
Teodor <thelad43@gmail.com>
2017-11-17 15:57:33 +0200

    Added solution for problem 05. Sequence 2k+1

---
+++ 07. Advanced Loops/05. Sequence2KPlus1/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace _05.Sequence2KPlus1
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var num = 1;
+
+            for (int i = 1; num <= n; i++)
+            {
+                Console.WriteLine(num);
+                num = num * 2 + 1;
+            }
+        }
+    }
+}
