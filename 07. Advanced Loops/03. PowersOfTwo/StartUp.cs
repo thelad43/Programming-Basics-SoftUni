58e75705372b4958b8338aee0b9398c1fe3b0873
Teodor <thelad43@gmail.com>
2017-11-17 15:55:27 +0200

    Added solution for problem 03. Powers of Two

---
+++ 07. Advanced Loops/03. PowersOfTwo/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _03.PowersOfTwo
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
