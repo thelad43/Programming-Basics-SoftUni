66e9d11518bad12a774aa0d90971373f2eb94c0a
Teodor <thelad43@gmail.com>
2017-11-17 15:58:47 +0200

    Added solution for problem 06. Number in Range [1...100]

---
+++ 07. Advanced Loops/06. NumberInRange1To100/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _06.NumberInRange1To100
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.Write("Еnter a number in the range [1...100]: ");
+            var n = int.Parse(Console.ReadLine());
+
+            if (n >= 1 && n <= 100)
+            {
+                Console.WriteLine($"The number is: {n}");
+            }
+            else
+            {
+                do
+                {
+                    Console.Write("Еnter a number in the range [1...100]: ");
+                    n = int.Parse(Console.ReadLine());
+
+                    if (n >= 1 && n <= 100)
+                    {
+                        Console.WriteLine($"The number is: {n}");
+                        return;
+                    }
+
+                } while (true);
+            }
+        }
+    }
+}
