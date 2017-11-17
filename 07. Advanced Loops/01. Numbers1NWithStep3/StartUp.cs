fdfebe7b94484790d1d92d464fa8ab15b0af412c
Teodor <thelad43@gmail.com>
2017-11-17 15:51:47 +0200

    Added 07. Advanced Loops solution and problem 01. Numbers 1...N with Step 3

---
+++ 07. Advanced Loops/01. Numbers1NWithStep3/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.Numbers1NWithStep3
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
