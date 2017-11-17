5c516e74c9ffb50536ca2f397df75ea3c6fbee27
Teodor <thelad43@gmail.com>
2017-11-17 16:13:59 +0200

    Added solution for problem 14. Number Table

--- 07. Advanced Loops/14. NumberTable/StartUp.cs
+++ 07. Advanced Loops/14. NumberTable/StartUp.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _14.NumberTable
+﻿namespace _14.NumberTable
 {
-    class Program
+    using System;
+
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    var num = row + col + 1;
+                    if (num > n)
+                        num = 2 * n - num;
+                    Console.Write($"{num} ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
