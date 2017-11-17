7bbf29636a12894145a64e772917539551e86006
Teodor <thelad43@gmail.com>
2017-11-17 16:09:14 +0200

    Added solution for problem 10. Check Prime

--- 07. Advanced Loops/10. CheckPrime/StartUp.cs
+++ 07. Advanced Loops/10. CheckPrime/StartUp.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _10.CheckPrime
+﻿namespace _10.CheckPrime
 {
-    class Program
+    using System;
+
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            var n = int.Parse(Console.ReadLine());
+            var isPrime = true;
+
+            if (n < 2)
+            {
+                Console.WriteLine("Not Prime");
+                return;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
         }
     }
 }
