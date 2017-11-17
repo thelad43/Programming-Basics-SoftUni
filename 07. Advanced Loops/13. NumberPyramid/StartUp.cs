35ec7ed0c87065f6ca73ad8b59adb993cf7cb833
Teodor <thelad43@gmail.com>
2017-11-17 16:12:00 +0200

    Added solution for problem 13. Number Pyramid

---
+++ 07. Advanced Loops/13. NumberPyramid/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace _13.NumberPyramid
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var number = 1;
+            var counter = 1;
+
+            while (number <= n)
+            {
+                for (int i = 0; i < counter; i++)
+                {
+                    Console.Write(number + " ");
+                    number++;
+
+                    if (number > n)
+                    {
+                        return;
+                    }
+                }
+
+                counter++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
