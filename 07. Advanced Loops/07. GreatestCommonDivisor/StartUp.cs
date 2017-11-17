91ec5793835b2b1b88ff291bac167d498e144afb
Teodor <thelad43@gmail.com>
2017-11-17 15:59:50 +0200

    Added solution for problem 07. Greatest Common Divisor (CGD)

---
+++ 07. Advanced Loops/07. GreatestCommonDivisor/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _07.GreatestCommonDivisor
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+
+            while (b != 0)
+            {
+                var temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
