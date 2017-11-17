5eefa838d50c608581d6220fb48690825ddba429
Teodor <thelad43@gmail.com>
2017-11-17 15:54:10 +0200

    Added solution for problem 02. Numbers N...1

---
+++ 07. Advanced Loops/02. NumbersN1/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.NumbersN1
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
