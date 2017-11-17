b7024e73f8fd2f85cdf8daa60b8f668b9ab918b1
Teodor <thelad43@gmail.com>
2017-11-17 16:10:54 +0200

    Added solution for problem 12. Fibonacci

---
+++ 07. Advanced Loops/12. Fibonacci/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _12.Fibonacci
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var f0 = 1;
+            var f1 = 1;
+
+            if (n < 2)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                var nextFibNum = f0 + f1;
+                f0 = f1;
+                f1 = nextFibNum;
+            }
+
+            Console.WriteLine(f1);
+        }
+    }
+}
