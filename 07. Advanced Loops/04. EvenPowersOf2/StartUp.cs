6ac252bacb0786fa06d79a98cd01523a66b35173
Teodor <thelad43@gmail.com>
2017-11-17 15:56:27 +0200

    Added solution for problem 04. Even Powers of 2

---
+++ 07. Advanced Loops/04. EvenPowersOf2/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.EvenPowersOf2
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i += 2)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
