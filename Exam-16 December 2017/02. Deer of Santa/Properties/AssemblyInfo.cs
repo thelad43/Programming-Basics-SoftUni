86bc30e8be758dbeb86621dbe826ec7fc7c6167c
Teodor <thelad43@gmail.com>
2017-12-22 13:34:01 +0200

    Added solution for problem 02. Deer of Santa

--- Exam-16 December 2017/02. Deer of Santa/Properties/AssemblyInfo.cs
+++ Exam-16 December 2017/02. Deer of Santa/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("01. Christmas Preparation")]
+[assembly: AssemblyTitle("02. Deer of Santa")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("01. Christmas Preparation")]
+[assembly: AssemblyProduct("02. Deer of Santa")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b1035302-a248-4c85-ad0e-b223e6085be9")]
+[assembly: Guid("dca8c37a-6152-469d-9fce-9d7be50dae0e")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Exam-16 December 2017/02. Deer of Santa/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace _02.Deer_of_Santa
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var daysSantaDoesntWork = int.Parse(Console.ReadLine());
+            var leftFoodInKg = int.Parse(Console.ReadLine());
+            var foofPerDayForFirstDeer = double.Parse(Console.ReadLine());
+            var foofPerDayForSecondDeer = double.Parse(Console.ReadLine());
+            var foofPerDayForThirdDeer = double.Parse(Console.ReadLine());
+
+            var neededFoodForFirstDeer = daysSantaDoesntWork * foofPerDayForFirstDeer;
+            var neededFoodForSecondDeer = daysSantaDoesntWork * foofPerDayForSecondDeer;
+            var neededFoodForThirdDeer = daysSantaDoesntWork * foofPerDayForThirdDeer;
+
+            var totalFoodInKg = neededFoodForFirstDeer + neededFoodForSecondDeer + neededFoodForThirdDeer;
+
+            if (leftFoodInKg >= totalFoodInKg)
+            {
+                Console.WriteLine($"{Math.Floor(leftFoodInKg - totalFoodInKg)} kilos of food left.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Ceiling(totalFoodInKg - leftFoodInKg)} more kilos of food are needed.");
+            }
+        }
+    }
+}
