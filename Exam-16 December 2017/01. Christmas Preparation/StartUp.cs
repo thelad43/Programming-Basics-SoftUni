3a1894b3c864f12807774bfcdb55332914b183e9
Teodor <thelad43@gmail.com>
2017-12-22 13:23:23 +0200

    Added Programming Basics Exam - 16 December 2017 solution and solution for problem 01. Christmas Preparation

---
+++ Exam-16 December 2017/01. Christmas Preparation/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.Christmas_Preparation
+{
+    using System;
+
+    public class StartUp
+    {
+        private const double wrappingPaperPriceInLevs = 5.80;
+        private const double materialPriceInLevs = 7.20;
+        private const double gluePricePerLiterInLevs = 1.20;
+
+        public static void Main()
+        {
+            var wrappingPaperRollsCount = int.Parse(Console.ReadLine());
+            var materialRollsCount = int.Parse(Console.ReadLine());
+            var glueLiters = double.Parse(Console.ReadLine());
+            var discountPercentages = int.Parse(Console.ReadLine());
+
+            var priceWrappingPaperRolls = wrappingPaperRollsCount * wrappingPaperPriceInLevs;
+            var priceMaterialRolls = materialRollsCount * materialPriceInLevs;
+            var priceGlue = glueLiters * gluePricePerLiterInLevs;
+
+            var priceForAllMaterials = priceWrappingPaperRolls + priceMaterialRolls + priceGlue;
+            var priceWithDiscount = priceForAllMaterials - ((priceForAllMaterials * discountPercentages) / 100);
+
+            Console.WriteLine($"{priceWithDiscount:F3}");
+        }
+    }
+}
